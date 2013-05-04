13fa1a83b876d7b141e0cd38e044980c879fbb63
Clara Bayarri <nayami.ruka@gmail.com>
2013-05-04 10:47:20 +0200

    Spring mongo config

--- src/main/java/com/crowdplatform/config/MongoConfig.java
+++ src/main/java/com/crowdplatform/config/MongoConfig.java
@@ -4,21 +4,45 @@ import java.net.UnknownHostException;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 
 import com.mongodb.DB;
+import com.mongodb.Mongo;
 import com.mongodb.MongoException;
 import com.mongodb.MongoURI;
 
 @Configuration
-public class MongoConfig {
-	@SuppressWarnings("deprecation")
+public class MongoConfig extends AbstractMongoConfiguration {
+	
 	@Bean
 	public DB getDb() throws UnknownHostException, MongoException {
-		MongoURI mongoURI = new MongoURI(System.getenv("MONGOHQ_URL"));
+		MongoURI mongoURI = getMongoURI();
         DB db = mongoURI.connectDB();
         db.authenticate(mongoURI.getUsername(), mongoURI.getPassword());
     
         return db;
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Bean
+	public Mongo mongo() throws UnknownHostException {
+	return new Mongo(getMongoURI());
+	}
+	 
+	@Override
+	public String getDatabaseName() {
+	return getMongoURI().getDatabase();
+	}
+	 
+	@Bean
+	public SimpleMongoDbFactory mongoDbFactory() throws Exception {
+	return new SimpleMongoDbFactory(getMongoURI());
+	}
+	 
+	@SuppressWarnings("deprecation")
+	private MongoURI getMongoURI() {
+	return new MongoURI(System.getenv("MONGOHQ_URL"));
 	}
 
 }
--- src/main/java/com/crowdplatform/service/MongoService.java
+++ src/main/java/com/crowdplatform/service/MongoService.java
@@ -1,13 +1,12 @@
 package com.crowdplatform.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Service;
-
-import com.mongodb.DB;
 
 @Service
 public class MongoService {
 
 	@Autowired
-	DB mongoDB;
+	MongoOperations mongoOperation;
 }
